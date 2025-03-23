185dedd6f60231105cbc6d29e8d848617950337a
benjaminz <benjatumwesigye@gmail.com>
2025-03-23 09:39:11 +0200

    test file

--- test_ota.py
+++ test_ota.py
@@ -3,13 +3,13 @@ from utils import load_config
 
 config = load_config()
 
-firmware_url = "https://github.com/kevinmcaleer/ota_test/main/"
+firmware_url = "https://github.com/benjamintumwesigye/ota/"
 
 # Load Wi-Fi credentials
 if config:
     ssid = config.get('ssid')
     password = config.get('password')
 
-    ota_updater = OTAUpdater(ssid, password, firmware_url, "test_ota.py")
+    ota_updater = OTAUpdater(ssid, password, firmware_url, "test_file.py")
 
     ota_updater.download_and_install_update_if_available()
